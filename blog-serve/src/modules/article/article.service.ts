a356eb7b302c69f85b310bf982e4b13b754f6309
Jeek <zkkysqs@gmail.com>
2025-04-22 22:23:11 +0800

    chore: format

--- blog-serve/src/modules/article/article.service.ts
+++ blog-serve/src/modules/article/article.service.ts
@@ -1,22 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { CreateArticleDto } from './dto/create-article.dto';
 import { UpdateArticleDto } from './dto/update-article.dto';
-import {Article} from "./entities/article.entity";
+import { Article } from './entities/article.entity';
 
 @Injectable()
 export class ArticleService {
   list: Article[] = [];
   constructor() {
-    this.list.push(Article.fromJson({
-      id: 1,
-      title: '标题1',
-      description: '描述1',
-      content: '内容1',
-      createTime: new Date(),
-      updateTime: new Date(),
-      author: 'admin',
-      tags: ['标签1', '标签2'],
-    }));
+    this.list.push(
+      Article.fromJson({
+        id: 1,
+        title: '标题1',
+        description: '描述1',
+        content: '内容1',
+        createTime: new Date(),
+        updateTime: new Date(),
+        author: 'admin',
+        tags: ['标签1', '标签2'],
+      }),
+    );
   }
   create(createArticleDto: CreateArticleDto) {
     const article = Article.fromJson({
@@ -38,7 +40,7 @@ export class ArticleService {
   }
 
   findOne(id: number) {
-    return this.list.find(item => item.id === id);
+    return this.list.find((item) => item.id === id);
   }
 
   update(id: number, updateArticleDto: UpdateArticleDto) {
@@ -57,7 +59,7 @@ export class ArticleService {
   remove(id: number) {
     const article = this.findOne(id);
     if (article) {
-      const index = this.list.findIndex(item => item.id === id);
+      const index = this.list.findIndex((item) => item.id === id);
       this.list.splice(index, 1);
       return article;
     } else {
