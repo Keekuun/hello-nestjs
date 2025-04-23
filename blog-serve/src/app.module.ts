f1a9cd138483de92a2d79af8967ea05d7a74900d
jeek <zkkysqs@gmail.com>
2025-04-23 17:05:28 +0800

    chore: add postgresql

--- blog-serve/src/app.module.ts
+++ blog-serve/src/app.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ArticleModule } from './modules/article/article.module';
+import {TypeOrmModule} from "@nestjs/typeorm";
+
+const PostgresqlModule = TypeOrmModule.forRoot({
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: '123456',
+  database: 'nestjs-blog-serve',
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  synchronize: true,
+});
 
 @Module({
-  imports: [ArticleModule],
+  imports: [
+    PostgresqlModule,
+    ArticleModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
