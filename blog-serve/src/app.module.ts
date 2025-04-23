14d72b9515070d5c87e336ebe269ea7187003454
jeek <zkkysqs@gmail.com>
2025-04-23 20:33:43 +0800

    feat: use envFile

--- blog-serve/src/app.module.ts
+++ blog-serve/src/app.module.ts
@@ -1,17 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ArticleModule } from './modules/article/article.module';
 import {TypeOrmModule} from "@nestjs/typeorm";
 import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 
+// Load ConfigModule which assists with environment variables
+ConfigModule.forRoot({ envFilePath: '.env' });
+
 const DatabaseModule = TypeOrmModule.forRoot({
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'root',
-  password: '123456',
-  database: 'nestjs-blog-serve',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT as string, 10),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
   synchronize: true,
   namingStrategy: new SnakeNamingStrategy(),
